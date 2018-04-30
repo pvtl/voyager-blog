710589a8b7ec55f7b424d32e5d857e9c23ccab91
Jean-Pierre Gassin <jeanpierre.gassin@gmail.com>
2018-04-30 15:42:56 +1000

    Make sure we can use both the frontend and regular blog views

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,16 @@
 <?php
 
+$namespace = '\Pvtl\VoyagerBlog\Http\Controllers';
+
+if (class_exists('\Pvtl\VoyagerFrontend\Http\Controllers\PostController')) {
+    $namespace = '\Pvtl\VoyagerFrontend\Http\Controllers';
+}
+
 Route::group([
     'prefix' => 'posts', // Must match its `slug` record in the DB > `data_types`
     'middleware' => ['web'],
     'as' => 'voyager-blog.posts.',
-    'namespace' => '\Pvtl\VoyagerBlog\Http\Controllers'
+    'namespace' => $namespace,
 ], function () {
     Route::get('/', ['uses' => 'PostController@getPosts', 'as' => 'list']);
     Route::get('{slug}', ['uses' => 'PostController@getPost', 'as' => 'post']);
